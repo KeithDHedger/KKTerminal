0c2f74d9dff7d3bb5c35df9f75c9a05ecbf89f5d
keithhedger <keithhedger@8ae0b8bd-d74c-4b99-b23b-1209d3cb34e7>
2016-07-14 15:53:54 +0000

    git-svn-id: file:///home/keithhedger/SVN/KKTerminal@13 8ae0b8bd-d74c-4b99-b23b-1209d3cb34e7

--- KKTerminal/src/gui.cpp
+++ KKTerminal/src/gui.cpp
@@ -337,7 +337,7 @@ void buildMainGui(void)
 	gtk_notebook_set_scrollable((GtkNotebook*)mainNotebook,true);
 
 	gtk_notebook_set_show_tabs((GtkNotebook*)mainNotebook,true);
-	g_object_set_data(G_OBJECT(mainNotebook),"homogeneous",(gpointer)true);
+	g_object_set_data(G_OBJECT(mainNotebook),"tab-expand",(gpointer)true);
 
 //menus
 	menuBar=gtk_menu_bar_new();
